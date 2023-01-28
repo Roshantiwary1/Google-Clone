75c60bde9eac702f7ce0c6f3599c89ecf0e9f088
Roshantiwary1 <tiwariroshan9999@gmail.com>
2023-01-28 22:55:01 +0530

    protected the data coming from api

--- components/ImageResults.js
+++ components/ImageResults.js
@@ -8,8 +8,8 @@ const ImageResults = ({results}) => {
         {results.items.map(result=>(
             <div key={result.link}>
                 <div className="group mb-10 ml-1">
-                    <a href={result.image.contextLink}><img className='group-hover:shadow-xl h-60 object-contain w-full' src={result.link} alt={result.title} /></a>
-                    <a className='group-hover:underline' href={result.image.contextLink}><h2 className='truncate text-xl'>{result.title}</h2></a>
+                    <a href={result.image.contextLink}><img className='group-hover:shadow-xl h-60 object-contain w-full' src={result?.link} alt={result?.title} /></a>
+                    <a className='group-hover:underline' href={result.image.contextLink}><h2 className='truncate text-xl'>{result?.title}</h2></a>
                     <a href={result.image.contextLink}><p className='text-gray-500'>{result.displayLink}</p></a>
                 </div>
             </div>
