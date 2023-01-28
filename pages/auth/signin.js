7ae2cb44965b831fb63230dc82f4d562b8850e4a
Roshantiwary1 <tiwariroshan9999@gmail.com>
2023-01-28 23:19:37 +0530

    final

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -9,7 +9,7 @@ const signin = ({Providers}) => {
       <div className="mt-7 flex justify-center">
         {Object.values(Providers).map((provider)=>(
             <div key={provider.name} className="flex justify-center flex-col items-center">
-            <Image width="550" height="150" src="https://www.pngall.com/wp-content/uploads/5/Google-Logo-PNG-Free-Image.png" alt="google" />
+            <Image width="550" height="150" className='mt-[35px]' src="https://www.pngall.com/wp-content/uploads/5/Google-Logo-PNG-Free-Image.png" alt="google" />
                 <p className='text-sm italic mb-10 text-center text-gray-600'>this app is created for learning purposes</p>
                 <button onClick={()=>(signIn(provider.id , {callbackUrl:"/"}))} className="bg-blue-500 rounded-lg text-white p-3 hover:bg-blue-600">SignIn with {provider.name}</button>
             </div>
