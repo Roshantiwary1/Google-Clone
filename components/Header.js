b62b3d6f3bb793465ff309b26ee48f680126bbe8
Roshantiwary1 <tiwariroshan9999@gmail.com>
2023-01-28 22:37:48 +0530

    added secret to authenticate

--- components/Header.js
+++ components/Header.js
@@ -4,12 +4,12 @@ const Header = () => {
   return (
     <header className='flex justify-between sm:p-5 p-2 text-sm text-gray-700'>
      <div className="flex sm:space-x-5 space-x-2 items-center">
-        <p className='link'>About</p>
-        <p className='link'>Store</p>
+        <a href='https://about.google/' target="_blank" rel="noreferrer" className='link'>About</a>
+        <a href='https://store.google.com/' target="_blank" rel="noreferrer" className='link'>Store</a>
      </div>
      <div className="flex sm:space-x-4 space-x-2 items-center">
-        <p className='link'>Gmail</p>
-        <p className='link'>Images</p>
+        <a href='https://mail.google.com' target="_blank" rel="noreferrer" className='link'>Gmail</a>
+        <a  target="_blank" rel="noreferrer" className='link'>Images</a>
         <User/>
      </div>
     </header>
